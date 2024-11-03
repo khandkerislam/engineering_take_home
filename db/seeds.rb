f87936437bdeb5e61916bda1698aaf34c0690101
Khandker Islam <khandker.islam46@gmail.com>
2024-11-03 11:19:28 -0500

    feat: created seeds file for building and client data
    Created a seeds file to populate the database with initial data, no custom fields yet.
    
    Resolves: #seeds-file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Client.destroy_all
+Building.destroy_all
+
+va_state = State.find_or_create_by!(name: 'Virginia', code: 'VA')
+va_zip_code = ZipCode.find_or_create_by!(code: 22312, state: va_state)
+
+# Create 5 clients
+clients = [
+  {
+    name: 'Acme Properties',
+    buildings: [
+      { address: '123 Beauregard St', zip_code: va_zip_code },
+      { address: '456 Duke St', zip_code: va_zip_code }
+    ]
+  },
+  {
+    name: 'Skyline Realty',
+    buildings: [
+      { address: '789 Seminary Rd', zip_code: va_zip_code },
+      { address: '321 King St', zip_code: va_zip_code }
+    ]
+  },
+  {
+    name: 'Urban Dwellings',
+    buildings: [
+      { address: '555 Little River Tpke', zip_code: va_zip_code }
+    ]
+  }
+].map do |client_info|
+  client = Client.create!(name: client_info[:name])
+
+  client_info[:buildings].each do |building_info|
+    Building.create!(address: building_info[:address], zip_code: building_info[:zip_code], client: client)
+  end
+end
