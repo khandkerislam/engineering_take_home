da06e28d92dbb124d8a26d103c21fe0d23802bb0
Khandker Islam <khandker.islam46@gmail.com>
2024-11-05 09:56:49 -0500

    refactor: add seeds file for custom fields and building custom values
    Created a complete seeds file to populate the database with initial data for custom fields and building custom values belonging to proper clients and buildings.
    
    Resolves: #seeds-file

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,5 +1,7 @@
 class Building < ApplicationRecord
     belongs_to :client
     belongs_to :zip_code
+
+    has_many :building_custom_values, dependent: :destroy
     validates :address, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,38 +8,49 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Client.destroy_all
-Building.destroy_all
+Rails.logger.info "Clearing existing data..."
+[Client, Building, CustomField, BuildingCustomValue].each(&:destroy_all)
 
+Rails.logger.info "Creating location data..."
 va_state = State.find_or_create_by!(name: 'Virginia', code: 'VA')
 va_zip_code = ZipCode.find_or_create_by!(code: 22312, state: va_state, city: 'Alexandria')
 
-# Create 5 clients
 clients = [
-  {
-    name: 'Acme Properties',
-    buildings: [
-      { address: '123 Beauregard St', zip_code: va_zip_code },
-      { address: '456 Duke St', zip_code: va_zip_code }
-    ]
-  },
-  {
-    name: 'Skyline Realty',
-    buildings: [
-      { address: '789 Seminary Rd', zip_code: va_zip_code },
-      { address: '321 King St', zip_code: va_zip_code }
-    ]
-  },
-  {
-    name: 'Urban Dwellings',
-    buildings: [
-      { address: '555 Little River Tpke', zip_code: va_zip_code }
-    ]
-  }
-].map do |client_info|
-  client = Client.create!(name: client_info[:name])
+  Client.new(name: 'Larchmont Properties'), 
+  Client.new(name: 'Skyline Realty'), 
+  Client.new(name: 'Landmark Development')
+]
+buildings = [
+  Building.new(address: '123 Beauregard St', zip_code: va_zip_code, client: clients[0]), 
+  Building.new(address: '456 Duke St', zip_code: va_zip_code, client: clients[0]),
+  Building.new(address: '789 Seminary Rd', zip_code: va_zip_code, client: clients[1]),
+  Building.new(address: '321 King St', zip_code: va_zip_code, client: clients[1]),
+  Building.new(address: '555 Little River Tpke', zip_code: va_zip_code, client: clients[2])
+]
+custom_fields = [
+  CustomField.new(name: 'Number of Floors', field_type: :number, client: clients[0]), 
+  CustomField.new(name: 'Number of Units', field_type: :number, client: clients[0]),
+  CustomField.new(name: 'Building Type', field_type: :list, enum_options: ['Residential', 'Commercial', 'Mixed Use'], client: clients[1]),
+  CustomField.new(name: 'Year Built', field_type: :number, client: clients[2]),
+  CustomField.new(name: 'Historical Notes', field_type: :string, client: clients[2])
+]
+building_custom_values = [
+  BuildingCustomValue.new(building: buildings[0], custom_field: custom_fields[0], value: 3),
+  BuildingCustomValue.new(building: buildings[0], custom_field: custom_fields[1], value: 10),
+  BuildingCustomValue.new(building: buildings[1], custom_field: custom_fields[0], value: 4),
+  BuildingCustomValue.new(building: buildings[1], custom_field: custom_fields[1], value: 12),
+  BuildingCustomValue.new(building: buildings[2], custom_field: custom_fields[2], value: 'Residential'),
+  BuildingCustomValue.new(building: buildings[3], custom_field: custom_fields[2], value: 'Commercial'),
+  BuildingCustomValue.new(building: buildings[4], custom_field: custom_fields[3], value: 1950),
+  BuildingCustomValue.new(building: buildings[4], custom_field: custom_fields[4], value: 'Former bank building')
+]
 
-  client_info[:buildings].each do |building_info|
-    Building.create!(address: building_info[:address], zip_code: building_info[:zip_code], client: client)
-  end
+Rails.logger.info "Creating clients, buildings and custom fields..."
+ActiveRecord::Base.transaction do
+  clients.each(&:save!)
+  buildings.each(&:save!)
+  custom_fields.each(&:save!)
+  building_custom_values.each(&:save!)
 end
+
+Rails.logger.info "Seed data created successfully!"
