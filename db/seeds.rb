81739e41420ed318970b91b231cbd7e1a73be5e5
Khandker Islam <khandker.islam46@gmail.com>
2024-11-06 13:56:11 -0500

    refactor: seeds file now loads five clients

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,51 +22,66 @@ ca_zip_code = ZipCode.find_or_create_by!(code: 90001, state: ca_state, city: 'Lo
 clients = [
   Client.new(name: 'Larchmont Properties'), 
   Client.new(name: 'Skyline Realty'), 
-  Client.new(name: 'Landmark Development')
-]
-buildings = [
-  *(20.times.map { |i| Building.new(address: "#{100 + i} Madison Ave", zip_code: ny_zip_code, client: clients[0]) }),
-  *(20.times.map { |i| Building.new(address: "#{100 + i} Beauregard St", zip_code: va_zip_code, client: clients[1]) }),
-  *(20.times.map { |i| Building.new(address: "#{100 + i} Hollywood Blvd", zip_code: ca_zip_code, client: clients[2]) }),
+  Client.new(name: 'Landmark Development'),
+  Client.new(name: 'Richmond Properties'),
+  Client.new(name: 'Oakland Properties')
 ]
 
 custom_fields = [
   CustomField.new(name: 'Number of Floors', field_type: :number, client: clients[0]), 
   CustomField.new(name: 'Number of Units', field_type: :number, client: clients[0]),
   CustomField.new(name: 'Building Type', field_type: :list, enum_options: ['Residential', 'Commercial', 'Mixed Use'], client: clients[1]),
+  CustomField.new(name: 'Square Footage', field_type: :number, client: clients[1]),
   CustomField.new(name: 'Year Built', field_type: :number, client: clients[2]),
-  CustomField.new(name: 'Historical Notes', field_type: :string, client: clients[2])
+  CustomField.new(name: 'Historical Notes', field_type: :string, client: clients[2]),
+  CustomField.new(name: 'Number of Bathrooms', field_type: :number, client: clients[3]),
+  CustomField.new(name: 'Number of Bedrooms', field_type: :number, client: clients[3]),
+  CustomField.new(name: 'Parking Spaces', field_type: :number, client: clients[4]),
+  CustomField.new(name: 'Roof Type', field_type: :list, enum_options: ['Asphalt', 'Tile', 'Slate'], client: clients[4])
 ]
-building_custom_values = [
-  # For NY buildings (clients[0]) - Number of Floors and Units
-  *(20.times.flat_map do |i|
-    [
-      BuildingCustomValue.new(building: buildings[i], custom_field: custom_fields[0], value: rand(3..10)),
-      BuildingCustomValue.new(building: buildings[i], custom_field: custom_fields[1], value: rand(10..50))
-    ]
-  end),
+buildings = []
+building_custom_values = []
 
-  # For VA buildings (clients[1]) - Building Type
-  *(20.times.map do |i|
-    BuildingCustomValue.new(
-      building: buildings[i + 20], 
-      custom_field: custom_fields[2], 
-      value: ['Residential', 'Commercial', 'Mixed Use'].sample
-    )
-  end),
+100.times do |i|
+  client_index = i / 20 
+  base_field_index = client_index * 2 
 
-  # For CA buildings (clients[2]) - Year Built and Historical Notes
-  *(20.times.flat_map do |i|
-    [
-      BuildingCustomValue.new(building: buildings[i + 40], custom_field: custom_fields[3], value: rand(1920..2000)),
-      BuildingCustomValue.new(
-        building: buildings[i + 40], 
-        custom_field: custom_fields[4], 
-        value: ["Former bank building", "Historic landmark", "Renovated in 2010", "Original facade"].sample
-      )
+  case client_index
+  when 0  # Larchmont Properties - New York
+    building = Building.new(address: "#{rand(100..999)} Madison Ave", zip_code: ny_zip_code, client: clients[0])
+    values = [
+      BuildingCustomValue.new(building: building, custom_field: custom_fields[base_field_index], value: rand(3..10)),
+      BuildingCustomValue.new(building: building, custom_field: custom_fields[base_field_index + 1], value: rand(10..50))
     ]
-  end)
-]
+  when 1  # Skyline Realty - Virginia
+    building = Building.new(address: "#{rand(1000..1999)} Beauregard St", zip_code: va_zip_code, client: clients[1])
+    values = [BuildingCustomValue.new(building: building, custom_field: custom_fields[base_field_index], 
+      value: ['Residential', 'Commercial', 'Mixed Use'].sample)]
+  when 2  # Landmark Development - California
+    building = Building.new(address: "#{rand(5000..5999)} Hollywood Blvd", zip_code: ca_zip_code, client: clients[2])
+    values = [
+      BuildingCustomValue.new(building: building, custom_field: custom_fields[base_field_index], value: rand(1920..2000)),
+      BuildingCustomValue.new(building: building, custom_field: custom_fields[base_field_index + 1], 
+        value: ["Former bank building", "Historic landmark", "Renovated in 2010", "Original facade"].sample)
+    ]
+  when 3  # Richmond Properties - California
+    building = Building.new(address: "#{rand(200..499)} N 12th St", zip_code: ca_zip_code, client: clients[3])
+    values = [
+      BuildingCustomValue.new(building: building, custom_field: custom_fields[base_field_index], value: rand(1..5)),
+      BuildingCustomValue.new(building: building, custom_field: custom_fields[base_field_index + 1], value: rand(1..5))
+    ]
+  when 4  # Oakland Properties - New York
+    building = Building.new(address: "#{rand(300..699)} E 16th St", zip_code: ny_zip_code, client: clients[4])
+    values = [
+      BuildingCustomValue.new(building: building, custom_field: custom_fields[base_field_index], value: rand(1..5)),
+      BuildingCustomValue.new(building: building, custom_field: custom_fields[base_field_index + 1], 
+        value: ['Asphalt', 'Tile', 'Slate'].sample)
+    ]
+  end
+
+  buildings << building
+  building_custom_values.concat(values)
+end
 
 Rails.logger.info "Creating clients, buildings and custom fields..."
 ActiveRecord::Base.transaction do
